8cdc1b8d57d0433e43f06b49317c9f410b77b280
Arseny Biryukov <arsbir@yandex-team.ru>
2026-03-28 07:50:15 +0300

    G6 add connect wo cert

--- scripts/group06/demo.sh
+++ scripts/group06/demo.sh
@@ -15,6 +15,13 @@ pause
 
 less -p node.crt ~/ydb-setup/3-nodes-mirror-3-dc/files/config.yaml
 ssh yandex-ydb-1 sudo cat /opt/ydb/certs/node.crt | less
+
+comment "Для подключения нужно обязательно указать корневой сертификат, которым подписаны сертификаты узлов"
+comment "Обычная команда подключения:"
+comment "ydb --endpoint grpcs://entrypoint.ydb-cluster.com:2135 -d /Root/database --user root --ca-file ~/ca.crt"
+comment "Попробуем подключиться без сертификата:"
+run "ydb --endpoint grpcs://entrypoint.ydb-cluster.com:2135 -d /Root/database --user petrov@ldap"
+
 pause
 
 header "КРИТЕРИЙ 35: Наличие инструментов прозрачного шифрования данных на уровне хранения"
