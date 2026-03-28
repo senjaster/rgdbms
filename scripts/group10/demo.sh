88d5a477c6652460067c620de1a5bb822c66020c
Arseny Biryukov <arsbir@yandex-team.ru>
2026-03-28 11:33:51 +0300

    G9 add C45

--- scripts/group10/demo.sh
+++ scripts/group10/demo.sh
@@ -52,22 +52,30 @@ pause
 
 comment "Запустим запрос в фоновом режиме, подключаясь к yandex-ydb-1:"
 run "ydb -p default -e grpcs://yandex-ydb-1.ydb-cluster.com:2135 sql -s \"SELECT count(*) FROM item i1 CROSS JOIN item i2\" &"
-pause
 
+comment ""
 comment "Проверим с помощью netstat, к какому хосту установлено соединение:"
-run "netstat -an | grep :2137"
+run "netstat -a | grep :2137"
+comment "Видно, что соединение установлено не к yandex-ydb-1"
+pkill -f "SELECT count.*FROM item"
 pause
-
-comment "Остановим фоновый процесс:"
-run "pkill -f \"SELECT count.*FROM item\""
-pause
-
-
 
 # ============================================
+
 header "КРИТЕРИЙ 45: Автоматическое переключение на резервный узел"
 
-comment "Переключение"
-
+comment "Сам по себе discovery не поможет если узел полностью выйдет из строя"
+comment "Остановим процесс хранения на первом узле"
+pause
+run "ssh yandex-ydb-1 sudo systemctl stop ydbd-storage"
+comment "Сейчас мы фактически сымитировали полную недоступность одного из узлов кластера"
+comment ""
+comment "Выполним discovery еще раз"
 pause
 
+run "ydb -p default -e grpcs://yandex-ydb-1.ydb-cluster.com:2135 discovery list"
+
+run "ydb -p default -e grpcs://entrypoint.ydb-cluster.com:2135 discovery list"
+
+ssh yandex-ydb-1 sudo systemctl start ydbd-storage
+ssh yandex-ydb-1 sudo systemctl start ydbd-database-a
