2fe7f1728bf0f5c8f64c4d1c6c324ec0d443dbff
Arseny Biryukov <arsbir@yandex-team.ru>
2026-03-28 07:11:13 +0300

    Improve group 5

--- scripts/group05/demo.sh
+++ scripts/group05/demo.sh
@@ -7,17 +7,24 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # КРИТЕРИЙ 25: Возможность использования внешнего провайдера аутентификации
 # ============================================
 
+# Подготовка
+ydb -p default sql -s 'REVOKE SELECT ON `/Root/database/item` FROM `cn=ydb_role1,ou=groups,dc=ydb-cluster,dc=com@ldap`'
+
+
 header "КРИТЕРИЙ 25: Возможность использования внешнего провайдера аутентификации"
 
-comment "YDB поддерживает интеграцию с внешними провайдерами аутентификации, включая LDAP."
-comment "Демонстрация настройки и использования LDAP аутентификации:"
+comment "YDB поддерживает интеграцию c LDAP. Не предъявляется требований к конкретной реализации - "
+comment "поддерживается любой сервер, реализующий стандартный протокол."
+comment "Поддерживается LDAPS и 
 comment ""
 
 pause
 
-less -p ldap_authentication ~/ydb-setup/3-nodes-mirror-3-dc/files/config.yaml
+link "https://ydb.tech/docs/ru/security/authentication?version=v25.2#ldap"
 
-scriptreplay -t "${SCRIPT_DIR}/criteria25_timingfile.tm" -m 2 -d 2 "${SCRIPT_DIR}/criteria25_session.log"
+less -p ldap_authentication: ~/ydb-setup/3-nodes-mirror-3-dc/files/config.yaml
+
+run "ydb --endpoint grpcs://entrypoint.ydb-cluster.com:2135 -d /Root/database --ca-file ~/ca.crt --user petrov@ldap"
 
 pause
 
@@ -28,9 +35,11 @@ pause
 
 header "КРИТЕРИЙ 18: Возможность интеграции графической консоли с внешней системой аутентификации"
 
-comment "Графическая консоль YDB поддерживает интеграцию с внешними системами аутентификации."
-comment "Пользователи могут входить в веб-интерфейс используя учетные данные из LDAP."
+comment "Веб-интерфейс YDB использует для аутентификации те же механизмы что и при подключении через GRPC,"
+comment "поэтому он автоматически поддерживает LDAP."
 comment ""
+
+link "https://10.40.13.21:8765/monitoring/cluster/tenants?showHealthcheck=1"
 
 pause
 
@@ -47,9 +56,20 @@ comment ""
 
 pause
 
-scriptreplay -t "${SCRIPT_DIR}/criteria27_timingfile.tm" -m 3 -d 1 "${SCRIPT_DIR}/criteria27_session.log"
+comment "Выполним запрос от имени пользователя ivanov@ldap:"
+run ydb -p default --user ivanov@ldap sql -s "SELECT * FROM item LIMIT 10"
 
-# Очистка (команда из old/27/run.sh)
-ydb -p default --user root sql -s 'REVOKE SELECT ON `/Root/database/item` FROM `cn=ydb_role1,ou=groups,dc=ydb-cluster,dc=com@ldap`' 2>/dev/null
+comment "Теперь выдадим права группе ydb_role1 в которую он входит"
+run ydb -p default --user root sql -s 'GRANT SELECT ON `/Root/database/item` TO `cn=ydb_role1,ou=groups,dc=ydb-cluster,dc=com@ldap`'
+
+comment "Запустим запрос повторно:"
+run ydb -p default --user ivanov@ldap sql -s "SELECT * FROM item LIMIT 10"
+
+comment "По-умолчанию YDB использует группы, в которые пользователь входит непосредственно,"
+commetn "однако можно включить и рекурсивный поиск групп"
+link "https://ydb.tech/docs/ru/security/authentication?version=v25.2#poluchenie-grupp"
 
 pause
+
+# Очистка (команда из old/27/run.sh)
+ydb -p default --user root sql -s 'REVOKE SELECT ON `/Root/database/item` FROM `cn=ydb_role1,ou=groups,dc=ydb-cluster,dc=com@ldap`'
