8fd8aa49ca59eba40919437f7fc8781956a28a90
Arseny Biryukov <arsbir@yandex-team.ru>
2026-03-30 17:36:27 +0300

    Add test4

---
+++ scripts/ydb-bench/test_tpcc_like.sh
@@ -0,0 +1,12 @@
+export YDB_PASSWORD='PASSw0rd!!!'
+
+ydb-bench \
+    --endpoint grpcs://entrypoint.ydb-cluster.com:2135 \
+    --database /Root/database \
+    --ca-file ~/ca.crt \
+    --user root \
+    --prefix-path bench \
+    --scale 1000 \
+    run
+    --jobs 10
+    --file tpcc_like.sql
---
+++ scripts/ydb-bench/tpcc_like.sql
@@ -0,0 +1,6 @@
+UPDATE pgbench_accounts SET abalance = abalance + $delta WHERE aid = $aid;
+SELECT abalance FROM pgbench_accounts WHERE aid = $aid;
+UPDATE pgbench_tellers SET tbalance = tbalance + $delta WHERE tid = $tid;
+UPDATE pgbench_branches SET bbalance = bbalance + $delta WHERE bid = $bid;
+INSERT INTO pgbench_history (tid, bid, aid, delta, mtime)
+VALUES ($tid, $bid, $aid, $delta, CurrentUtcTimestamp());`
