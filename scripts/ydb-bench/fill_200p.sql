280bb7406408153b345935026cc39bdcc11ba0c5
Arseny Biryukov <arsbir@yandex-team.ru>
2026-03-30 16:57:41 +0300

    Fix fill

--- scripts/ydb-bench/fill_200p.sql
+++ scripts/ydb-bench/fill_200p.sql
@@ -2,7 +2,7 @@ $maxaid = SELECT max(aid) FROM `bench/accounts_200p`;
 
 REPLACE INTO `bench/accounts_200p`(aid, bid, abalance, filler)
 SELECT aid+$maxaid, aid+$maxaid, 0, null
-FROM  `bench/accounts_200p`
-WHERE aid BETWEEN 1 AND 500000;
+FROM  `bench/accounts`
+WHERE aid BETWEEN 1 AND 1000000;
 
 
